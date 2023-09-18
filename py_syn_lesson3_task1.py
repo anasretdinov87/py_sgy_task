7a33986e6403e5a5d75809c6398f8ce5d5eb2166
anasretdinov87 <ya@anasretdinov.ru>
2023-09-18 22:39:27 +0300

    first task lesson 3 to be done

--- py_syn_lesson3_task1.py
+++ py_syn_lesson3_task1.py
@@ -1,1 +1,10 @@
+print("Enter pet type:")
+kind_of_pet = input()
 
+print("Enter your pet's age:")
+pet_age = input()
+
+print("Enter your pet's name:")
+pet_name = input()
+
+print("This is a " + kind_of_pet + " named " + pet_name + ". Age:" + pet_age + " years old. ")
