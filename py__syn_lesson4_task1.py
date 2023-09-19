12a269f24245005d79f3dadc0f7e17a2c66cac8f
anasretdinov87 <ya@anasretdinov.ru>
2023-09-19 07:49:57 +0300

    les4 task1 added

---
+++ py__syn_lesson4_task1.py
@@ -0,0 +1,8 @@
+print("Enter side A of the rectangle:")
+a = float(input())
+print("Enter side B of the rectangle:")
+b = float(input())
+p = 2 * a + 2 * b
+s = a * b
+print("Square: " + str(s))
+print("Perimeter: " + str(p))
