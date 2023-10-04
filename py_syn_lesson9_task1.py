6a84e2318f680012ee3ed213383ad4f839c8730a
anasretdinov87 <ya@anasretdinov.ru>
2023-10-04 22:43:13 +0300

    les9 task 1 rework and task 3 added

--- py_syn_lesson9_task1.py
+++ py_syn_lesson9_task1.py
@@ -1,25 +1,35 @@
 check_count = True
 while check_count:
     print("Введите количество чисел (1 <= N <= 100000)")
-    n = int(input())
-    if n >= 1 and n <= 100000:
-        check_count = False
+    n = input()
+    if n.isnumeric():
+        n = int(n)
+        if n >= 1 and n <= 100000:
+            check_count = False
 
 check_nums = True
+
 while check_nums:    
     nums_arr = []
     print(f"Введите через пробел числа, количество - {n} (Числа не должны превышать 2*10e9 по модулю):")
     nums_arr = input().split(' ')
-    check_errors = True
-    if len(nums_arr) < n or len(nums_arr) > n:
-        print("Ошибка. Неверное число чисел. Повторите ввод.")
-        check_errors = False
-    else:
-        for i in range(len(nums_arr)):
-            if abs(int(nums_arr[i])) > 2*10e9:
-                print(f"Ошибка. Число номер {i + 1} по модулю превышает 2*10е9. Повторите ввод")
-                check_errors = False
-                break
+    check_nums = True
+    check_errors = True 
+    for i in range(len(nums_arr)):
+        if not nums_arr[i].isnumeric():
+            print(f"Ошибка. Введено не число. Повторите ввод")
+            check_errors = False
+            break
+    if check_errors:       
+        if len(nums_arr) < n or len(nums_arr) > n:
+            print("Ошибка. Неверное число чисел. Повторите ввод.")
+            check_errors = False
+        else:
+            for i in range(len(nums_arr)):
+                if abs(int(nums_arr[i])) > 2*10e9:
+                    print(f"Ошибка. Число номер {i + 1} по модулю превышает 2*10е9. Повторите ввод")
+                    check_errors = False
+                    break
     if check_errors:
         check_nums = False
 diff_nums = set(nums_arr)
