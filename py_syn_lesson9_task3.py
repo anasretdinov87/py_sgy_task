42a12ea391474c3c24f618bfde369b508c2a0d43
anasretdinov87 <ya@anasretdinov.ru>
2023-10-07 22:06:59 +0300

    lesson 9 task 3 to be done

--- py_syn_lesson9_task3.py
+++ py_syn_lesson9_task3.py
@@ -1,2 +1,27 @@
-print("Введите последовательность чисел через пробел:")
-nums_arr
+nums_check = True
+while nums_check:
+    print("Введите последовательность чисел через пробел:")
+    nums_arr = []
+    nums_arr = input().split(' ')
+    for i in range(len(nums_arr)):
+        nums_check = False
+        if not nums_arr[i].isnumeric():
+            print("Ошибка. Введено не число. Повторите ввод.")
+            nums_check = True
+            break
+
+result_arr = []
+s = set()
+for i in range(len(nums_arr)):
+    if i == 0:
+        result_arr.append("NO")
+        s.add(nums_arr[i])
+        continue
+    if nums_arr[i] in s:
+        result_arr.append("YES")
+    else:
+        result_arr.append("NO")
+        s.add(nums_arr[i])
+print(result_arr)
+
+
