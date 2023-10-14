337eb7d1629449d44ae842b182831193ca5bca19
anasretdinov87 <ya@anasretdinov.ru>
2023-10-14 09:23:01 +0300

    added lesson 13 task 1

--- py_syn_lesson13_task1.py
+++ py_syn_lesson13_task1.py
@@ -27,4 +27,23 @@ matrix1 = [[0] * m for i in range(n)]
 for i in range(m):
     for j in range(n):
         matrix1[i][j] = random.randrange(-200, 201)
+
+matrix2 = [[0] * m for i in range(n)]
+for i in range(m):
+    for j in range(n):
+        matrix2[i][j] = random.randrange(-200, 201)
+
+matrix_sum = [[0] * m for i in range(n)]
+for i in range(m):
+    for j in range(n):
+        matrix_sum[i][j] = matrix1[i][j] + matrix2[i][j]
+
+print('Матрица 1:')
 pl(matrix1)
+print()
+print('Матрица 2:')
+print()
+pl(matrix2)
+print()
+print('Результат сложение:')
+pl(matrix_sum)
