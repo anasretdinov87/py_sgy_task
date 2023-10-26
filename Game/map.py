e9c1d63268e37b5d9b310a3064b83a26e4265356
anasretdinov87 <ya@anasretdinov.ru>
2023-10-26 08:55:07 +0300

    Some bugs have been fixde

--- Game/map.py
+++ Game/map.py
@@ -1,5 +1,6 @@
 from utils import randbool
 from utils import randcell
+from utils import randcell2
 #0 - field
 #1 - tree
 #2 - river
@@ -11,7 +12,18 @@ class Map:
         rc = randcell(self.w, self.h)
         rx, ry = rc[0], rc[1]
         self.cells[rx][ry] = 2
-    
+        sum_free_cells = 0
+        for i in range(self.w):
+            for j in range(self.h):
+                if self.cells[i][j] != 2:
+                    sum_free_cells += 1
+        while l > 0:
+            rc2 = randcell2(rx, ry)
+            rx2, ry2 = rc2[0], rc2[1]
+            if (self.check_bounds(rx2, ry2)):
+                self.cells[rx2][ry2] = 2
+                rx, ry = rx2, ry2
+                l -= 1
     def generate_forest(self, r, mxr):
         for ri in range(self.h):
             for ci in range(self.w):
@@ -46,7 +58,9 @@ class Map:
         self.cells = [[0 for i in range(w)] for j in range(h)]
 
 
-tmp = Map(10, 10)
+tmp = Map(9, 10)
+tmp.generate_forest(3, 10)
 tmp.generate_river(10)
-#tmp.generate_forest(1, 10)
+tmp.generate_river(10)
+tmp.generate_river(10)
 tmp.print_map()
--- Game/utils.py
+++ Game/utils.py
@@ -5,13 +5,15 @@ def randbool(r, mxr):
     return (t <= r)
 
 def randcell(w, h):
-    tw = rand(0, w)
-    th = rand(0, h)
+    tw = rand(0, w - 1)
+    th = rand(0, h - 1)
     return(th, tw)
 
 #0 - up, 1 -right, 2 - down, 3 - left
 def randcell2(x, y):
     moves = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-    t = rand(0, 4)
+    t = rand(0, 3)
     dx, dy = moves[t][0], moves[t][1]
     return (x + dx, y + dy)
+
+
